6454a1621eed12b98d4febdb432be3a871b03e5b
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-01-09 09:07:50 +0300

    Proje dosyası ekle.

---
+++ BasicMvvmWithDevExpress/ViewModels/MainViewModel.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.Native
+
+Public Class MainViewModel
+    Inherits ViewModelBase
+
+    'Commands
+    Public Property GetAllCommand As ICommand
+    'Props
+    Public Property Cars As DXObservableCollection(Of Car)
+
+    Public Sub New()
+        GetAllCommand = New DelegateCommand(AddressOf GetAllCar)
+    End Sub
+
+    Public Sub GetAllCar()
+        Cars = New CarService().GetAllCar
+        RaisePropertiesChanged("Cars")
+    End Sub
+End Class
